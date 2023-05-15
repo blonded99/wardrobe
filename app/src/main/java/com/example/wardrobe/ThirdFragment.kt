ec711d691f2e4f51f019abe257bf6e12b1051711
blonded99 <didls2654@naver.com>
2023-05-15 22:29:53 +0900

    갤러리에서 옷 추가하기 전 상의/하의 선택 분기하는 custom dialog 추가

--- app/src/main/java/com/example/wardrobe/ThirdFragment.kt
+++ app/src/main/java/com/example/wardrobe/ThirdFragment.kt
@@ -1,9 +1,13 @@
 package com.example.wardrobe
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.example.wardrobe.databinding.FragmentThirdBinding
 
@@ -21,6 +25,36 @@ class ThirdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonCamera.setOnClickListener {
+            val dialog = context?.let { it1 -> Dialog(it1) }
+
+            // dialog 적용할 layout
+            dialog?.setContentView(R.layout.custom_dialog_gallery)
+
+            // dialog 모서리 둥글게
+            dialog?.window?.setBackgroundDrawableResource(R.drawable.dialog_rounding)
+
+            // dialog size 설정
+            dialog?.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+
+            // dialog 화면 밖 터치 액션
+            dialog.setCanceledOnTouchOutside(true)
+            dialog.setCancelable(true)
+
+            val topButton = dialog.findViewById<ImageButton>(R.id.button_top)
+            val bottomButton = dialog.findViewById<ImageButton>(R.id.button_bottom)
+
+            topButton.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            bottomButton.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
     }
 
     override fun onResume() {
