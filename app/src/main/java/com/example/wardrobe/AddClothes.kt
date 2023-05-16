5ea0f8bfa95aca839978f8e050616e90b2216a5a
JYeyun <117142316+JYeyun@users.noreply.github.com>
2023-05-16 15:43:36 +0900

    xml 추가(2)

---
+++ app/src/main/java/com/example/wardrobe/AddClothes.kt
@@ -0,0 +1,8 @@
+package com.example.wardrobe
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wardrobe.databinding.FragmentAddclothesBinding
+
+class AddClothes : AppCompatActivity(){
+
+}
