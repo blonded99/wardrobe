250d117d2fa12917ed07b08c27778d95e9141f8a
blonded99 <didls2654@naver.com>
2023-05-23 01:40:52 +0900

    커뮤니티 좋아요 기능 구현

--- app/src/main/java/com/example/wardrobe/adapters/CommunityMainRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/wardrobe/adapters/CommunityMainRecyclerViewAdapter.kt
@@ -6,11 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wardrobe.R
+import com.example.wardrobe.viewmodel.CommunityItem
 import com.example.wardrobe.viewmodel.CommunityViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
@@ -20,6 +23,9 @@ class CommunityMainRecyclerViewAdapter(private val viewModel: CommunityViewModel
     RecyclerView.Adapter<CommunityMainRecyclerViewAdapter.RecyclerViewViewHolder>() {
 
     private lateinit var storage: FirebaseStorage
+
+    // 회원가입 구현 시 이부분 firebase auth에서 받아올 것
+    val currentUID = "3t6Dt8DleiZXrzzf696dgF15gJl2"
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(
@@ -46,6 +52,7 @@ class CommunityMainRecyclerViewAdapter(private val viewModel: CommunityViewModel
         val setColRef = db.collection("set")
 
         private val clothesImage: ImageView = itemView.findViewById(R.id.iv_clothes)
+        private val buttonLike: ImageButton = itemView.findViewById(R.id.button_like)
 
         fun setContents(pos: Int){
             with(viewModel.communityMainItems[pos]){
@@ -59,10 +66,25 @@ class CommunityMainRecyclerViewAdapter(private val viewModel: CommunityViewModel
                     }
             }
 
-
-            clothesImage.setOnClickListener {
-
+            buttonLike.setOnClickListener {
+                setColRef.whereEqualTo("imageRef",viewModel.communityMainItems[pos].clothesImageUrl).get()
+                    .addOnSuccessListener {
+                        for(doc in it){
+                            val tempList = doc["likedUser"] as MutableList<String> ?: mutableListOf()
+                            if (tempList.contains(currentUID)) {
+                                tempList.remove(currentUID)
+                                setColRef.document(doc.id).update("likedUser",tempList)
+                                return@addOnSuccessListener
+                            }
+                            else {
+                                tempList.add(currentUID)
+                                setColRef.document(doc.id).update("likedUser",tempList)
+                                return@addOnSuccessListener
+                            }
+                        }
+                    }
             }
+
         }
 
     }
