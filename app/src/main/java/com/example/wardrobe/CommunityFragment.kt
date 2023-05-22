379892b5e8bd70f2e66b78b43cdcca79bbfae1a9
blonded99 <didls2654@naver.com>
2023-05-22 22:25:30 +0900

    코디 추가 후 옷장으로 네비게이션 이동이 되지 않던 문제 해결

--- app/src/main/java/com/example/wardrobe/CommunityFragment.kt
+++ app/src/main/java/com/example/wardrobe/CommunityFragment.kt
@@ -35,8 +35,6 @@ class CommunityFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        viewModel.addCommunityItem(Item(R.drawable.test_top))
-//        viewModel.addCommunityItem(Item(R.drawable.test_bottom))
 
         val adapter = CommunityRecyclerViewAdapter(viewModel,context,this)
 
