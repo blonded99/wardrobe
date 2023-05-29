1f79c0eff7052762199e6e769806b84173235bde
Serim Son <serim711@gmail.com>
2023-05-30 06:26:41 +0900

    fix: api parsing error

--- app/src/main/java/com/example/wardrobe/network/model/WeatherNowResponse.kt
+++ app/src/main/java/com/example/wardrobe/network/model/WeatherNowResponse.kt
@@ -7,15 +7,15 @@ import kotlinx.serialization.Transient
 @Serializable
 data class WeatherNowResponse(
     val coord: Coordinate,
-    val weathers: List<WeatherData>,
+    val weather: List<WeatherData>,
     @Transient val base: String = "", // Internal parameter
     val main: MainForecast,
     val visibility: UInt,
     val wind: WindData,
     @Transient val clouds: Map<String, Int>? = null,
-    val rain: PrecipitationData?,
-    val snow: PrecipitationData?,
-    val dt: UInt,
+    val rain: PrecipitationData? = null,
+    val snow: PrecipitationData? = null,
+    val dt: ULong,
     val sys: SunData,
     val timezone: Int, // Shift in seconds from UTC
     val id: Int,
@@ -37,20 +37,20 @@ data class WeatherData(
     val description: String,
     val icon: String,
 ) {
-    fun toWeather(): Weather = when(id) {
+    fun toWeather(): Weather = when (id) {
         in 200 until 300 -> Weather.THUNDER_STORM
         in 300 until 400 -> Weather.DRIZZLE
-        in 500..504,in 520..522, 531  -> Weather.RAINY
+        in 500..504, in 520..522, 531 -> Weather.RAINY
         in 600 until 700, 511 -> Weather.SNOWY
         701 -> Weather.MIST
         711 -> Weather.SMOKE
         721 -> Weather.HAZE
-        731,761->Weather.DUST
-        741->Weather.FOG
-        751->Weather.SAND
-        762->Weather.ASH
-        771-> Weather.SQUALL
-        781->Weather.TORNADO
+        731, 761 -> Weather.DUST
+        741 -> Weather.FOG
+        751 -> Weather.SAND
+        762 -> Weather.ASH
+        771 -> Weather.SQUALL
+        781 -> Weather.TORNADO
         800 -> Weather.CLEAR
         in 801 until 900 -> Weather.CLOUDY
         else -> Weather.NA
@@ -65,21 +65,21 @@ data class MainForecast(
     val temp_max: Float,
     val humidity: UInt,
     val pressure: Int,
-    val sea_level: Int?,
-    val grnd_level: Int?,
+    val sea_level: Int? = null,
+    val grnd_level: Int? = null,
 )
 
 @Serializable
 data class WindData(
     val speed: Float,
     val deg: Int,
-    val gust: Float?,
+    val gust: Float? = null,
 )
 
 @Serializable
 data class PrecipitationData(
-    val `1h`: UInt?,
-    val `3h`: UInt?,
+    val `1h`: UInt? = null,
+    val `3h`: UInt? = null,
 )
 
 @Serializable
